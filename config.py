b84b30f11edf58e014ece7bf23792bdd6abab211
Akggautam <125729082+Akggautam@users.noreply.github.com>
2023-02-19 08:22:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,9 +32,9 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Akgsarchive:<password>@cl
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001773890991"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "6149334974"))
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "0"))
 
 #start message
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
@@ -64,9 +64,9 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(2127437034)
+ADMINS.append()
 
-LOG_FILE_NAME = "Akgsarchive"
+LOG_FILE_NAME = ""
 
 logging.basicConfig(
     level=logging.INFO,
